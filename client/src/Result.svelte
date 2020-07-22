98d9f37a27584c47d1b2c1872ea679964c33c337
Hablaklaflap <fkkrjakonzo@gmail.com>
2020-07-22 09:49:43 +0200

    Svelte solution

---
+++ client/src/Result.svelte
@@ -0,0 +1,34 @@
+<script>
+	import { fade, slide, scale } from 'svelte/transition';
+	import { flip } from 'svelte/animate';
+	export let sunrise;
+	export let sunset;
+</script>
+
+<div in:slide class="results">
+	<div class="one_result">
+		<img src="/images/sunrise.svg"  alt="Sunrise icon">
+		{sunrise}
+	</div>
+	<div class="one_result">
+		<img src="/images/sunset.svg"  alt="Sunset icon">
+		{sunset}
+	</div>
+</div>
+
+<style>
+	.results{
+		width: 400px;
+		margin: 0 auto;
+		text-align: center;
+	}
+	img{
+		width: 100px;
+		vertical-align:middle;
+		padding-right: 25px;
+	}
+	.one_result{
+		margin: 18px auto;
+	}
+
+</style>
